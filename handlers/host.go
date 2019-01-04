3bb6bbe6184a7a726b1b1ba51b59c7075559fd49
Mark Chmarny <mark@chmarny.com>
2019-01-04 07:09:03 -0800

    Format

--- handlers/host.go
+++ handlers/host.go
@@ -19,10 +19,12 @@ func hostHandler(w http.ResponseWriter, r *http.Request) {
 	info, infoErr := host.Info()
 
 	if infoErr == nil {
-		request = append(request, fmt.Sprintf("Node Host ID: %v", info.HostID))
+		request = append(request, "Host")
+		request = append(request, fmt.Sprintf("   Node ID: %v", info.HostID))
 		request = append(request, fmt.Sprintf("   Boot time: %v", time.Unix(int64(info.BootTime), 0)))
-		request = append(request, fmt.Sprintf("   Hostname: %v", info.Hostname))
 		request = append(request, fmt.Sprintf("   OS: %v", info.OS))
+		request = append(request, "Pod:")
+		request = append(request, fmt.Sprintf("   ID: %v", info.Hostname))
 	}
 
 	fmt.Fprintf(w, strings.Join(request, "\n"))
