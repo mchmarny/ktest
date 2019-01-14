fdff7f80ba3efbc4cbb8dc7a51c0103874516820
Mark Chmarny <mark@chmarny.com>
2019-01-14 15:54:17 -0800

    Added cgroups file writtable check

--- handlers/resource.go
+++ handlers/resource.go
@@ -1,7 +1,7 @@
 package handlers
 
 import (
-	"github.com/mchmarny/tellmeall/utils"
+
 	"log"
 	"net/http"
 	"time"
@@ -12,12 +12,13 @@ import (
 	"fmt"
 
 	"github.com/shirou/gopsutil/mem"
-
 	"github.com/shirou/gopsutil/cpu"
-
 	"github.com/shirou/gopsutil/host"
 
 	"github.com/mchmarny/tellmeall/types"
+	"github.com/mchmarny/tellmeall/utils"
+
+	"golang.org/x/sys/unix"
 )
 
 const (
@@ -60,7 +61,8 @@ func resourceHandler(w http.ResponseWriter, r *http.Request) {
 	vm, err := mem.VirtualMemory()
 	if err == nil {
 		sr.Node.Resources.Memory.Value = float64(vm.Total)
-		sr.Node.Resources.Memory.Context = "Source: OS process status"
+		sr.Node.Resources.Memory.Context = fmt.Sprintf("Source: OS process status, Size: %s",
+			utils.ByteSize(vm.Total))
 	}
 
 	count, err := cpu.Counts(true)
@@ -69,23 +71,23 @@ func resourceHandler(w http.ResponseWriter, r *http.Request) {
 		sr.Node.Resources.CPU.Context = "Source: OS process status"
 	}
 
-	// pod
-	val, ctx := getResourceLimit(limitMemResourceFile)
+	// pod memory
+	val, wr, ctx := getCGroupsFile(limitMemResourceFile)
 	sr.Pod.Limits.Memory.Value = val
-	sr.Pod.Limits.Memory.Context = fmt.Sprintf("%s; %s", ctx, utils.ByteSize(uint64(val)))
+	sr.Pod.Limits.Memory.Context = fmt.Sprintf("%s, Writable: %v, Size: %s", ctx, wr,
+		utils.ByteSize(uint64(val)))
 
-	quotaVal, quotaCtx := getResourceLimit(limitCPUQuotaResourceFile)
-	periodVal, _ := getResourceLimit(limitCPUPeriodResourceFile)
+	// pod cpu (calculated: quota / period)
+	quotaVal, wr, quotaCtx := getCGroupsFile(limitCPUQuotaResourceFile)
+	periodVal, _, _ := getCGroupsFile(limitCPUPeriodResourceFile)
 
+	// always context
+	sr.Pod.Limits.CPU.Context = fmt.Sprintf("%s, Writable: %v", quotaCtx, wr)
+
+	// value only if there is data
 	if quotaVal > 0 && periodVal > 0 {
-		sr.Pod.Limits.CPU = &types.SimpleIntMeasurement{
-			Value: quotaVal / periodVal,
-			Context: quotaCtx,
-		}
-	}else {
-		sr.Pod.Limits.CPU.Context = quotaCtx
+		sr.Pod.Limits.CPU.Value = quotaVal / periodVal
 	}
-
 
 	writeJSON(w, sr)
 
@@ -94,23 +96,23 @@ func resourceHandler(w http.ResponseWriter, r *http.Request) {
 
 
 
-func getResourceLimit(path string) (val float64, info string) {
+func getCGroupsFile(path string) (val float64, wr bool, info string) {
 
 	if _, err := os.Stat(path); os.IsNotExist(err) {
 		log.Printf("File[%s]: not found: %v", path, err)
-		return 0, fmt.Sprintf("File not found: %s", path)
+		return 0, false, fmt.Sprintf("File not found: %s", path)
 	}
 
 	file, err := os.Open(path)
 	if err != nil {
 		log.Printf("Error opening file[%s]: %v", path, err)
-		return 0, fmt.Sprintf("Unable to open file: %s", path)
+		return 0, false, fmt.Sprintf("Unable to open file: %s", path)
 	}
 
 	bc, err := ioutil.ReadAll(file)
 	if err != nil {
 		log.Printf("Error reading file[%s]: %v", path, err)
-		return 0, fmt.Sprintf("Unable to read file: %s", path)
+		return 0, false, fmt.Sprintf("Unable to read file: %s", path)
 	}
 
 	cs :=  strings.Trim(string(bc), "\n")
@@ -118,11 +120,11 @@ func getResourceLimit(path string) (val float64, info string) {
 	ic, err := strconv.ParseFloat(cs, 64)
 	if err != nil {
 		log.Printf("Error parsing content[%s]: %v", path, err)
-		return 0, fmt.Sprintf("Non-numeric value: %s", cs)
+		return 0, false, fmt.Sprintf("Non-numeric value: %s", cs)
 	}
 
 	log.Printf("File[%s]: %v = %f", path, cs, ic)
 
-	return ic, fmt.Sprintf("Source: %s", path)
+	return ic, unix.Access(path, unix.W_OK) == nil, fmt.Sprintf("Source: %s", path)
 
 }
